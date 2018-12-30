b8542388d3cb70bbd57160bfc97d04aa2cfbe6b8
Tahkus-Allen <tahkus.ekedal@gmail.com>
2018-12-30 10:22:32 -0800

    SideBar list filtering and alphabetizing added

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 
 class Filter extends Component {
 
-	state = {
-		query: ''
-	}
-
-	updateQuery = (query) => {
-		this.setState({ query: query.trim() }, this.props.filterParks(this.state.query))
-	}
+	
 
 	render() {
-		return (
-			<div className="filter-bar">
-				<div className="icon-wrapper">
-					<FontAwesomeIcon icon="search" className="search-icon"/>
-				</div>
-				<div className="filter-input">
-					<input type="text" value={this.state.query} placeholder="Enter search (e.g. Alvarado)" onChange={(e) => this.updateQuery(e.target.value)}/>
-				</div>
-			</div>
-		)
+		return
 	}
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import '../App.css';
 import ListItem from './ListItem.js'
 import Filter from './Filter.js'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class SideBar extends Component {
 
+	state = {
+			query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({ query: query.trim() })
+	}
+
   render() {
+  	let showingListItems
+  	if (this.state.query) {
+  		const match = new RegExp(escapeRegExp(this.state.query), 'i')
+  		showingListItems = this.props.parks.filter((item) => match.test(item.title))
+  	} else {
+  		showingListItems = this.props.parks
+  	}
+
+  	showingListItems.sort(sortBy('title'))
+
 	return (
 	  <div className="side-bar">
-	    <Filter filterParks={this.props.filterParks} parks={this.props.parks}/>
+	    <div className="filter-bar">
+				<div className="icon-wrapper">
+					<FontAwesomeIcon icon="search" className="search-icon"/>
+				</div>
+				<div className="filter-input">
+					<input type="text" value={this.state.query} placeholder="Enter search (e.g. Alvarado)" onChange={(e) => this.updateQuery(e.target.value)}/>
+				</div>
+			</div>
 	    <ol className="parks-list">
-		  {this.props.parks.map(park =>
+		  {showingListItems.map(park =>
 		    <ListItem key={park.id} park={park} onListClick={this.props.onListClick} />
 		  )}
 	    </ol>
